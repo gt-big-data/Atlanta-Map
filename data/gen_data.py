eb71a844759fa8bfa1aa720a46be7a4846611a74
Manley Roberts <manleyroberts@gatech.edu>
2018-11-05 01:50:53 -0500

    Added CherryPy server to test AJAX calls, added AJAX calls. Currently, CherryPy server just loads from static json, as BusClient.buses() endpoint is returning no data

--- data/gen_data.py
+++ data/gen_data.py
@@ -1,14 +1,38 @@
 from martapy import BusClient
+import json
+import cherrypy
+from cherrypy import tools
 
 '''
     In order to generate new json files, run this script.
+    Update: actually sets up a CherryPy server which simulates the server the frontend will be contacting
 '''
 
-bus_client = BusClient()
-buses = bus_client.buses()
+def error_page_404(status, message, traceback, version):
+    return "404 Error!"
 
+class HomeController():
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def GetBuses(self, **kwargs):
+        bus_client = BusClient()
+        # return bus_client.buses()
+        with open('new_data.json', 'r') as file:
+            buses = json.load(file)
+            oldfile = open("old_data.json", "w")
+            oldfile.write(buses.__repr__())
+            oldfile.close()
+            return buses
 
-file = open("new_data.json", "r")
+def start_server():
+    cherrypy.tree.mount(HomeController(), '/')
+    cherrypy.config.update({'error_page.404': error_page_404})
+    cherrypy.config.update({'server.socket_port': 8080})
+    cherrypy.config.update({'tools.response_headers.headers': [('Access-Control-Allow-Origin', 'http://localhost:8000')]})
+    cherrypy.config.update({'tools.response_headers.on': True})
+    cherrypy.engine.start()
 
-file.write(buses.__repr__())
-file.close()
+if __name__ == '__main__':
+    start_server()
+
+    # https://simpletutorials.com/c/2165/How%20to%20Create%20a%20Simple%20JSON%20Service%20with%20CherryPy
